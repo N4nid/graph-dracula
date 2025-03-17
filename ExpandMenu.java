5ce6954e60f041f61635b46cdd0e71382f4dfae5
MortySmith <63672127+Morty0Smith@users.noreply.github.com>
2025-03-17 22:19:43 +0100

    Continued work on expand menu

--- ExpandMenu.java
+++ ExpandMenu.java
@@ -1,13 +1,20 @@
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.layout.Pane;
 
 
 public class ExpandMenu {
     public Pane background;
+    public TextField mainInputField;
     private double xMargin = 60;
     private double height = 200;
     private TwoDVec<Double> standardButtonSize = new TwoDVec<Double>(99.875,52.0);
+    private double buttonDistXY = 20;
+    private int buttonColloms = 5;
+    private int buttonRows = 4;
+    private String standardPath = "/resources/MathIcons";
+    private String standardImageFormat = ".png";
     public ExpandMenu(Pane root) {
         background = new Pane();
         background.getStyleClass().add("black");
@@ -16,6 +23,24 @@ public class ExpandMenu {
         background.setPrefHeight(height);
         background.setViewOrder(-1);
         root.getChildren().add(background);
+        MathButton testButton = new MathButton(this,"/resources/MathIcons/e.png","e",0,standardButtonSize);
+    }
+
+    private void initiateButtons() {
+        TwoDVec<Double> currentPos = new TwoDVec<Double>(buttonDistXY,buttonDistXY);
+        MathButton fx = initiateButton("f(X)","f(x) = " , -6);
+        MathButton pi = initiateButton("pi","pi");
+        MathButton e = initiateButton("e","e");
+        MathButton phi = initiateButton("phi","phi");
+        MathButton square = initiateButton("square","^2");
+        MathButton exponent = initiateButton("exponent","^");
+    }
+
+    private MathButton initiateButton(String filename, String inputString) {
+        return new MathButton(this,standardPath + filename + standardImageFormat,inputString,0,standardButtonSize);
+    }
+    private MathButton initiateButton(String filename, String inputString, int cursorPos) {
+        return new MathButton(this,standardPath + filename + standardImageFormat,inputString,cursorPos,standardButtonSize);
     }
 }
 
@@ -25,13 +50,23 @@ class MathButton {
     private String inputString;
     private int cursorPos;
 
-    private Button baseButton;
+    public Button baseButton;
 
-    private MathButton(ExpandMenu parentMenu, String displayImagePath, String inputString, int cursorPos, TwoDVec<Double> pos, TwoDVec<Double> scale) {
+    public MathButton(ExpandMenu parentMenu, String displayImagePath, String inputString, int cursorPos, TwoDVec<Double> scale) {
         baseButton = new Button();
         baseButton.getStyleClass().add("black");
         baseButton.getStyleClass().add("border");
         baseButton.getStyleClass().add("image-button");
         baseButton.setStyle("-fx-background-image: url('" + displayImagePath + "');");
+        baseButton.getStyleClass().add("math-button");
+        baseButton.setPrefWidth(scale.x);
+        baseButton.setPrefHeight(scale.y);
+        Effects.addDefaultHoverEffect(baseButton);
+        this.inputString = inputString;
+        this.cursorPos = cursorPos;
+        this.parentMenu = parentMenu;
+        parentMenu.background.getChildren().add(baseButton);
     }
+
+
 }
