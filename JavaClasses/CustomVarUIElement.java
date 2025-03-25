0c09bc6b0e59a823b3cd64ce1e2279da4c24fb57
MortySmith <63672127+Morty0Smith@users.noreply.github.com>
2025-03-25 07:16:21 +0100

    Customized slider of CustomVarUIElement

--- JavaClasses/CustomVarUIElement.java
+++ JavaClasses/CustomVarUIElement.java
@@ -71,8 +71,8 @@ public class CustomVarUIElement {
         valueSlider.setPrefWidth(defaultWidth - defaultSliderWidthPadding*2);
         valueSlider.setLayoutX(defaultSliderWidthPadding);
         valueSlider.setLayoutY(defaultSliderHeightPadding + defaultHeight/2);
+        //valueSlider.setStyle("-fx-control-inner-background: ");
         //valueSlider.setBackground(new Background(new BackgroundFill(Color.BLACK, CornerRadii.EMPTY,new Insets(0))));
-        valueSlider.getStyleClass().add("default-slider");
         background.getChildren().add(valueSlider);
 
         parent.getChildren().add(background);
