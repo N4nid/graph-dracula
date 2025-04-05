cf24f93c488593930d63993408231a1d6bcf0b30
nanid <nanid@tuta.io>
2025-04-05 14:57:10 +0200

    added parsing support for parametic functions

--- JavaClasses/EquationTree.java
+++ JavaClasses/EquationTree.java
@@ -4,42 +4,45 @@ import java.util.ArrayList;
 
 import java.util.ArrayList;
 
-public class EquationTree{
-  
+public class EquationTree {
+
   public EquationNode root;
   public Color graphColor = Color.BLACK;
   public boolean isPreviewEquation = false;
   public boolean isFunction = false;
   public boolean isParametric = false;
+  public EquationNode intervalStart;
+  public EquationNode intervalEnd;
   public String name;
-  
+
   public EquationTree(byte rootState, String rootValue) {
-    this.root = new EquationNode(rootState,rootValue);
+    this.root = new EquationNode(rootState, rootValue);
   }
-  
+
   public EquationTree(byte rootState, double rootValue) {
-    this.root = new EquationNode(rootState,rootValue);
+    this.root = new EquationNode(rootState, rootValue);
   }
-  
-  public EquationTree(EquationNode root,String name, boolean isFunction) {
+
+  public EquationTree(EquationNode root, String name, boolean isFunction) {
     this.name = name;
     this.isFunction = isFunction;
     this.root = root;
   }
-  
+
   public EquationTree(EquationNode root) {
     this.root = root;
   }
-  
-  public EquationTree() {}
-  
+
+  public EquationTree() {
+  }
+
   public double calculate(TwoDVec<Double> coordinates, Variable[] customVariables, EquationTree[] existingFunctions) {
-    return root.calculate(coordinates,customVariables,existingFunctions);
+    return root.calculate(coordinates, customVariables, existingFunctions);
   }
-  
+
   public TwoDVec<Double> calculateParametrics(double t, Variable[] parameters) {
-    return (new TwoDVec<Double>(root.left.calculateParametric(t,parameters),root.right.calculateParametric(t,parameters)));
+    return (new TwoDVec<Double>(root.left.calculateParametric(t, parameters),
+        root.right.calculateParametric(t, parameters)));
   }
-  
-  
+
 }
