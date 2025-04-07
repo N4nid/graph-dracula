49cfd1d5d059b6500d6256ff0757dbe2aa3deaca
MortySmith <63672127+Morty0Smith@users.noreply.github.com>
2025-04-07 19:12:11 +0200

    Fixed parameters, ParametricsRenderer now Updates more frequently

--- JavaClasses/ParametricsRenderer.java
+++ JavaClasses/ParametricsRenderer.java
@@ -10,9 +10,6 @@ public class ParametricsRenderer {
   ArrayList<EquationTree> parametrics = new ArrayList<EquationTree>();
 
   private ApplicationController controller;
-  public TwoDVec<Double> lastPos = new TwoDVec<Double>(-1.0, -1.0);
-  public TwoDVec<Double> lastZoom = new TwoDVec<Double>(-1.0, -1.0);
-  ArrayList<ArrayList<TwoDVec<TwoDVec<Double>>>> parametricLineCache = new ArrayList<ArrayList<TwoDVec<TwoDVec<Double>>>>();
   
   public ParametricsRenderer(RenderValues renderValues, ApplicationController controller) {
     this.controller = controller;
@@ -41,7 +38,7 @@ public class ParametricsRenderer {
     return Math.cos(t)*(Math.pow(2.71828,Math.cos(t))-2*Math.cos(4*t)-Math.pow(Math.sin(1/12*t),5));      //Butterfly
   }
   
-  private ArrayList<ArrayList<TwoDVec<TwoDVec<Double>>>> calculateNewParametrics(boolean overrideCache) {
+  private ArrayList<ArrayList<TwoDVec<TwoDVec<Double>>>> calculateParametrics() {
     //zeichnet neue Funktionen aus negPosMaps in eine Pixelmap
     ArrayList<ArrayList<TwoDVec<TwoDVec<Double>>>> returnParametricsLines = new ArrayList<ArrayList<TwoDVec<TwoDVec<Double>>>>();
     double minLineLenght = 2;
@@ -65,38 +62,12 @@ public class ParametricsRenderer {
       //System.out.println(""+linePoints.size());
       returnParametricsLines.add(linePoints);
     }
-    if (overrideCache) {
-      parametricLineCache = returnParametricsLines;
-    }
     return returnParametricsLines;
   }
-  
-  public void moveGraphs(double DeltaX, double DeltaY) {
-    for (int i = 0; i < parametricLineCache.size(); i++) {
-      for (int j = 0; j < parametricLineCache.get(i).size(); j++) {
-        parametricLineCache.get(i).get(j).x.x += DeltaX;
-        parametricLineCache.get(i).get(j).x.y += DeltaY;
-        parametricLineCache.get(i).get(j).y.x += DeltaX;
-        parametricLineCache.get(i).get(j).y.y += DeltaY;
-      }
-    }
-  }
+
   
   public ArrayList<ArrayList<TwoDVec<TwoDVec<Double>>>> calculateParametricsLinePoints(ArrayList<EquationTree> parametrics) {
-    if (lastZoom.x != renderValues.zoom.x || lastZoom.y != renderValues.zoom.y) {
-      this.parametrics = parametrics;
-      this.lastZoom = new TwoDVec<Double>(renderValues.zoom.x, renderValues.zoom.y);
-      this.lastPos = new TwoDVec<Double>(renderValues.midpoint.x, renderValues.midpoint.y);
-      return calculateNewParametrics(true);
-    }
-    if(lastPos.x != renderValues.midpoint.x || lastPos.y != renderValues.midpoint.y) {
-      double DeltaX = (renderValues.midpoint.x - lastPos.x);
-      double DeltaY = (renderValues.midpoint.y - lastPos.y);
-      this.lastZoom = new TwoDVec<Double>(renderValues.zoom.x, renderValues.zoom.y);
-      this.lastPos = new TwoDVec<Double>(renderValues.midpoint.x, renderValues.midpoint.y);
-      moveGraphs(DeltaX,DeltaY);
-      return parametricLineCache;
-    }    
-    return parametricLineCache;
+    this.parametrics = parametrics;
+    return calculateParametrics();
   }
 }
